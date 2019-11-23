2555f4a70cf5cf1ed74338e3c3af9aa0676d5a16
Anatoly Yakovenko <aeyakovenko@gmail.com>
2019-11-22 23:00:11 -0800

    wip

--- benches/rayoff.rs
+++ benches/rayoff.rs
@@ -1,40 +1,43 @@
-use rayon::prelude::*;
-
 #![feature(test)]
 extern crate test;
+
+use rayon::prelude::*;
 
 #[bench]
 fn bench_pool(bencher: &mut Bencher) {
     let pool = Pool::new();
     bencher.iter(|| {
-        let mut array = [0usize ;100];
+        let mut array = [0usize; 100];
         pool.dispach_mut(&array, |val| *val += 1);
         let expected = [1usize; 100];
-        assert_eq!(array, expected);  
+        for i in 0..100 {
+            assert_eq!(array[i], expected[i]);
+        }
     })
 }
 
 #[bench]
 fn bench_baseline(bencher: &mut Bencher) {
     bencher.iter(|| {
-        let mut array = [0usize ;100];
+        let mut array = [0usize; 100];
         for i in array.iter_mut() {
             *i += 1;
         }
         let expected = [1usize; 100];
-        assert_eq!(array, expected);  
+        for i in 0..100 {
+            assert_eq!(array[i], expected[i]);
+        }
     })
 }
 
 #[bench]
 fn bench_rayon(bencher: &mut Bencher) {
     bencher.iter(|| {
-        let mut array = [0usize ;100];
-        array
-            .par_iter_mut()
-            .for_each(|p| *p += 1)
+        let mut array = [0usize; 100];
+        array.par_iter_mut().for_each(|p| *p += 1);
         let expected = [1usize; 100];
-        assert_eq!(array, expected);  
+        for i in 0..100 {
+            assert_eq!(array[i], expected[i]);
+        }
     })
 }
- 
--- src/rayoff.rs
+++ src/rayoff.rs
@@ -1,79 +1,85 @@
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::atomic::{Ordering, AtomicUsize};
+use std::mem::transmute;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::mpsc::{channel, Sender};
+use std::sync::{Arc, Condvar};
+use std::thread::spawn;
+use std::thread::JoinHandle;
 use sys_info;
 
 struct Job {
-    func: Fn(elems: *mut u64, index: usize)
-    elems: *mut u64;
-    num: usize;    
-    work_index: AtomicUsize;
-    done_index: AtomicUsize; 
+    func: fn(*mut u64, usize),
+    elems: *mut u64,
+    num: usize,
+    work_index: AtomicUsize,
+    done_index: AtomicUsize,
 }
 
 struct Pool {
-    senders: Vec<Sender<Arc<Job>>>
-    threads: Vec< JoinHandle<()>>,
-    signal: CondVar;
+    senders: Vec<Sender<Arc<Job>>>,
+    threads: Vec<JoinHandle<()>>,
+    signal: Condvar,
 }
 
 impl Job {
-    fn execute(&self, signal: &CondVar) {
+    fn execute(&self, signal: &Condvar) {
         loop {
-            let index = job.work_index.fetch_and_add(Ordering::Relaxed, 1);
-            if index >= job.num {
-                job.done_index.add(Ordering::Relaxed, 1);
+            let index = self.work_index.fetch_and_add(Ordering::Relaxed, 1);
+            if index >= self.num {
+                self.done_index.add(Ordering::Relaxed, 1);
                 signal.notify_one();
                 break;
             }
-            job.func(job.elems, index);
-        } 
+            self.func(self.elems, index);
+        }
     }
-    fn wait(&self, signal: &CondVar, num: usize) {
-        while job.done_index.fetch(Ordering::Relaxed) < num {
+    fn wait(&self, signal: &Condvar, num: usize) {
+        while self.done_index.fetch(Ordering::Relaxed) < num {
             signal.wait();
         }
     }
 }
 
 impl Pool {
-    pub fn new() -> {
+    pub fn new() -> Self {
         let num_threads = sys_info::cpu_num().unwrap_or(10);
-        let signal:  CondVar::new();
+        let signal = Condvar::new();
         let mut pool = Self {
             senders: vec![],
             threads: vec![],
             signal: signal.clone(),
-        }
-        (0 .. num_threads).for_each(|i|  {
-            let (sender, recevier) = channel();
-            let signal = signal.clone();
-            let t = spawn(|| {
-                for job in receiver.iter() {
-                    job.execute(&signal)
-                }
-            };
-            pool.senders.push(sender);
-            pool.threads.push(t);
-        }).collect();
+        };
+        (0..num_threads)
+            .for_each(|i| {
+                let (sender, recvr) = channel();
+                let signal = signal.clone();
+                let t = spawn(|| {
+                    for job in recvr.iter() {
+                        job.execute(&signal)
+                    }
+                });
+                pool.senders.push(sender);
+                pool.threads.push(t);
+            })
+            .collect();
         pool
     }
 
-    pub fn dispatch_mut<F, A, I>(&self, elems: &mut [A], func: F) 
-        where F: Fn(item: &mut A)
+    pub fn dispatch_mut<F, A, I>(&self, elems: &mut [A], func: F)
+    where
+        F: Fn(&mut A),
     {
         let job = Job {
-            #![allow(clippy::cast_ptr_alignment)]
-            elems: elems as *mut u64,
-            num: elems.len(), 
+            elems: elems.as_ptr_mut() as *mut u64,
+            num: elems.len(),
             work_index: AtomicUsize::new(0),
             done_index: AtomicUsize::new(0),
             func: |(ptr, index)| {
-                #![allow(clippy::cast_ptr_alignment)]
-                let elems = ptr as *mut A;
-                let items = elems as &mut [A];
+                let items = std::mem::transmute::<*mut u64, &mut [A]>(ptr);
+
                 func(&mut items[index])
-            }
-        }
+            },
+        };
+        let job = Arc::new(job);
         for s in &self.senders {
             s.send(job.clone());
         }
@@ -84,12 +90,15 @@ impl Pool {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn test_pool() {
         let pool = Pool::new();
-        let mut array = [0usize ;100];
+        let mut array = [0usize; 100];
         pool.dispach_mut(&array, |val| *val += 1);
         let expected = [1usize; 100];
-        assert_eq!(array, expected);
+        for i in 0..100 {
+            assert_eq!(array[i], expected[i]);
+        }
     }
 }
